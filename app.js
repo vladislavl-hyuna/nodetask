ba07fe8c78e726c574baf766374e50a787761d1e
Vladyslav Lisnyy <vladislav.lisniy@hyuna.bb>
2016-06-07 14:54:08 +0300

    CRUD

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var tasks = require("./data/tasks.json");
 var bodyParser = require("body-parser");
+var _ = require("lodash");
 
 app.set("views", "./views");
 app.set("view engine", "jade");
@@ -41,7 +42,45 @@ app.route('/task-list/add').get(function(req, res){
 
 app.get('/task-list/delete/:id', function(req, res){
     var taskId = req.params.id;
-    res.send(taskId);
+
+    tasks = tasks.filter(function(task){
+        return (task.id != taskId);
+    });
+
+    res.redirect('/task-list');
+});
+
+app.route('/task-list/edit/:id').get(function(req, res){
+    var taskId = req.params.id;
+
+    var task = _.find(tasks, function(task) {
+        return (task.id == taskId);
+    });
+
+    if(!task) {
+        res.sendStatus(404);
+        return;
+    }
+
+    res.render("edit", {
+        task
+    });
+}).post(function(req, res){
+    var taskId = req.params.id;
+
+    var task = _.find(tasks, function(task) {
+        return (task.id == taskId);
+    });
+
+    if(!task) {
+        res.sendStatus(404);
+        return;
+    }
+
+    task.task = req.body.task;
+    task.details = req.body.details;
+
+    res.redirect('/task-list');
 });
 
 app.listen(3000, function() {
